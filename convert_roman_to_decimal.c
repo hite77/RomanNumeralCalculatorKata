546a3b90d33de41701476668c87b8ecb0f347680
Jason Hite <hite77@gmail.com>
2016-08-06 21:12:35 -0400

    Added handling of 5, starting to add some structs to handle the next case.  Might have jumped the gun a little.

--- convert_roman_to_decimal.c
+++ convert_roman_to_decimal.c
@@ -1,6 +1,27 @@
 #include <string.h>
 
-int convert(char* roman_text)
+typedef struct 
 {
-	return strlen(roman_text);
+	int character;
+	int value;
+	int order;
+} roman_value_order;
+
+roman_value_order I={'I',1,1};
+roman_value_order V={'V',5,2};
+
+int convert(char roman_text[])
+{
+	int result = 0;
+	for(int i = 0; roman_text[i] != '\0'; i++) {
+    	if (I.character == roman_text[i])
+    	{
+    		result = result+I.value;
+    	}
+    	else
+    	{
+    		result = result+V.value;
+    	}
+    }
+	return result;
 }
--- test_convert_from_roman_numeral_to_decimal.c
+++ test_convert_from_roman_numeral_to_decimal.c
@@ -22,6 +22,13 @@ START_TEST(test_convert_III_to_decimal)
 }
 END_TEST
 
+START_TEST(test_convert_V_to_decimal)
+{
+	char roman_five[] = "V";
+	ck_assert_int_eq(convert(roman_five), 5);
+}
+END_TEST
+
 //Test for V so it knows that it is 5
 //Also Test X=10, L=fifty, C=1 hundred, D=five hundred, and M =1000
 //Test for IV should subtract one and be 4
@@ -40,6 +47,7 @@ int main(void)
     tcase_add_test(tc1_1, test_convert_I_to_decimal);
 	tcase_add_test(tc1_1, test_convert_II_to_decimal);
 	tcase_add_test(tc1_1, test_convert_III_to_decimal);
+	tcase_add_test(tc1_1, test_convert_V_to_decimal);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
