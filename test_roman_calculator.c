2a630cb6d8bb78b0ff8cf039923a1b7aa3f56269
Jason Hite <hite77@gmail.com>
2016-08-07 22:20:08 -0400

    Added some more adds, and subtract tests

--- test_roman_calculator.c
+++ test_roman_calculator.c
@@ -21,6 +21,48 @@ START_TEST(test_add_D_and_D)
 }
 END_TEST
 
+START_TEST(test_add_XIV_and_LX)
+{
+    add("XIV","LX",result_str);
+    ck_assert_str_eq(result_str, "LXXIV");
+}
+END_TEST
+
+START_TEST(test_add_XX_and_II)
+{
+    add("XX","II",result_str);
+    ck_assert_str_eq(result_str, "XXII");
+}
+END_TEST
+
+START_TEST(test_add_MCMXCIX_and_I)
+{
+    add("MCMXCIX","I",result_str);
+    ck_assert_str_eq(result_str, "MM");
+}
+END_TEST
+
+START_TEST(test_subtract_MCMXCIX_and_I)
+{
+    subtract("MCMXCIX","I",result_str);
+    ck_assert_str_eq(result_str, "MCMXCVIII");
+}
+END_TEST
+
+START_TEST(test_subtract_MM_and_I)
+{
+    subtract("MM","I",result_str);
+    ck_assert_str_eq(result_str, "MCMXCIX");
+}
+END_TEST
+
+START_TEST(test_subtract_CII_and_LV)
+{
+    subtract("CII","LV",result_str);
+    ck_assert_str_eq(result_str, "XLVII");
+}
+END_TEST
+
 int main(void)
 {
     Suite *s1 = suite_create("Core");
@@ -31,7 +73,13 @@ int main(void)
     suite_add_tcase(s1, test_roman_calculator);
     tcase_add_checked_fixture(test_roman_calculator, setup, teardown);
     tcase_add_test(test_roman_calculator, test_add_D_and_D);
-	
+    tcase_add_test(test_roman_calculator, test_add_XIV_and_LX);
+    tcase_add_test(test_roman_calculator, test_add_XX_and_II);
+	tcase_add_test(test_roman_calculator, test_add_MCMXCIX_and_I);
+    tcase_add_test(test_roman_calculator, test_subtract_MCMXCIX_and_I);
+    tcase_add_test(test_roman_calculator, test_subtract_MM_and_I);
+    tcase_add_test(test_roman_calculator, test_subtract_CII_and_LV);
+    
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
     srunner_free(sr);
