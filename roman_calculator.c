662aef1b87fa6324f5970f708da6ddcb17a2bbde
Jason Hite <hite77@gmail.com>
2016-08-11 23:59:31 -0400

    A few more refactors, and I think it is done again

--- roman_calculator.c
+++ roman_calculator.c
@@ -7,25 +7,33 @@
 int add_operation = 1;
 int subtract_operation = 0;
 
-void get_answer(return_type* return_value, char* first_roman_value, char* second_roman_value, int operation)
+void handle_errors(return_type* return_value,char* first_roman_value, char* second_roman_value, 
+	int integer_result)
 {
-	int integer_result;
-	if (operation == add_operation)
-	{
-		integer_result = convert_to_decimal(first_roman_value) + convert_to_decimal(second_roman_value);
-	}
-	else
-	{
-		integer_result = convert_to_decimal(first_roman_value) - convert_to_decimal(second_roman_value);
-	}
-	convert_to_roman(return_value->result_str, integer_result);
-
-	if ((convert_to_decimal(first_roman_value) == -1) || (convert_to_decimal(second_roman_value) == -1) || ((integer_result > 3999) || (integer_result <= 0)))
+	if ((convert_to_decimal(first_roman_value) == -1) || 
+	   (convert_to_decimal(second_roman_value) == -1) || 
+	   (integer_result > 3999) || 
+	   (integer_result <= 0))
 	{
 		return_value->error_code = 1;
 		strcpy(return_value->result_str, "INVALID");
-	}	
+	}
+}
 
+void get_answer(return_type* return_value, char* first_roman_value, char* second_roman_value, 
+	int operation)
+{
+	int integer_result = convert_to_decimal(first_roman_value);
+	if (operation == add_operation)
+	{
+		integer_result += convert_to_decimal(second_roman_value);
+	}
+	else
+	{
+		integer_result -= convert_to_decimal(second_roman_value);
+	}
+	convert_to_roman(return_value->result_str, integer_result);
+	handle_errors(return_value, first_roman_value, second_roman_value, integer_result);
 }
 
 void add(return_type* return_value, char* first_roman_value, char* second_roman_value)
